05a65ab431c24b18b2fe4a7341a600752dcb14cb
milemotigo <mtigo@gmail.com>
2023-09-29 06:10:38 -0700

    third

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,21 @@
-import {uploadPhoto, createUser} from "./utils.js";
+// import {uploadPhoto, createUser} from "./utils.js";
 
-export function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
+// export function handleProfileSignup() {
+//     return Promise.all([uploadPhoto(), createUser()])
 
-    .then(resp => {
-        const photos = resp[0];
-        const usr = resp[1];
-        console.log(`${photos.body} ${usr.firstName} ${usr.lastName}`); 
-    });
+//     .then(resp => {
+//         const photos = resp[0];
+//         const usr = resp[1];
+//         console.log(`${photos.body} ${usr.firstName} ${usr.lastName}`); 
+//     });
+// }
+
+import { uploadPhoto, createUser } from './utils';
+
+// const { firstName, lastName } = createUser();
+// const { body } = uploadPhoto();
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((result) => console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
