85545ec1cdf6d4a10ef8d4257a141531c3a82ab8
milemotigo <mtigo@gmail.com>
2023-09-29 09:39:15 -0700

    New commit

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,27 @@
+import { uploadPhoto, createUser } from "./utils";
+
+async function asyncUploadUser() {
+
+    try {
+        const photoResponse = await uploadPhoto();
+        const userResponse = await createUser()
+
+        return {
+            photo: photoResponse,
+            user: userResponse
+        };
+        
+    } catch (error) {
+        console.error("Error in asyncUploadUser:", error);
+        return {
+            photo: null,
+            user: null,
+        };
+    }
+}
+// const test = async () => {
+//     const value = await asyncUploadUser();
+//     console.log(value);
+// };
+
+// test();
