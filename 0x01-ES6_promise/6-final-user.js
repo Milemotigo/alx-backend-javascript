384243a091e8dab22c13fcfab71654f2470d7639
milemotigo <mtigo@gmail.com>
2023-09-29 14:45:52 -0700

    New commit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,44 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+// import signUpUser from "./4-user-promise";
+// import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-        .then((results) => {
-            const signUpResponse = results[0];
-            const photoResponse = results[1];
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//     return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+//         .then((results) => {
+//             const signUpResponse = results[0];
+//             const photoResponse = results[1];
 
-            return {
-                status: {
-                    signUp: signUpResponse.status,
-                    photo: photoResponse.status,
-                },
-                value: {
-                    signUp: signUpResponse.value,
-                    photo: photoResponse.value,
-                },
-            };
-        });
+//             return {
+//                 status: {
+//                     signUp: signUpResponse.status,
+//                     photo: photoResponse.status,
+//                 },
+//                 value: {
+//                     signUp: signUpResponse.value,
+//                     photo: photoResponse.value,
+//                 },
+//             };
+//         });
+// }
+
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  })).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  const photo = await uploadPhoto(fileName).then((value) => ({
+    status: 'fulfilled',
+    value,
+  })).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+
+  return [user, photo];
 }
