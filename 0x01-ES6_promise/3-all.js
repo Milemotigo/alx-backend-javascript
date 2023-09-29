448557755d3293fb26a1afc863017765f2ca0594
milemotigo <mtigo@gmail.com>
2023-09-29 06:24:10 -0700

    third

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,14 +3,11 @@
  export function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
 
-    .then((resp) => {
-         const photos = resp[0];
-         const usr = resp[1];
-         console.log(`${photos.body} ${usr.firstName} ${usr.lastName}`)
-     })
+    .then((resp) => console.log(`${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}`)
+    )
     .catch(() => console.log('Signup system offline'));
  }
-
+ handleProfileSignup();
 // import { uploadPhoto, createUser } from './utils';
 
 // export default function handleProfileSignup() {
