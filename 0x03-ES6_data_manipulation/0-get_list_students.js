5a0c5ac30c50ccb88b2bd6166a6be5a79d778e67
milemotigo <mtigo@gmail.com>
2023-10-02 16:01:10 +0100

    first commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents(){
+    const studentsInfo = []
+    let student1 = {id: 1, firstName: "Guillaume", location: "San Francisco"}
+    let student2 = {id: 2, firstName: "James", location: "Columbia"}
+    let student5 = {id: 5, firstName: "Serena", location: "San Francisco"}
+    studentsInfo.push(student1, student2, student5)
+    return studentsInfo
+}
