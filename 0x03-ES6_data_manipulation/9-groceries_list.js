0e869c0543f3c4a6e7830732409335df1741b898
milemotigo <mtigo@gmail.com>
2023-10-03 17:17:03 +0100

    New

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-    const groceriesMap = new Map();
-    groceriesMap.set("Apples", 10);
-    groceriesMap.set("Tomatoes", 10);
-    groceriesMap.set("Pasta", 1);
-    groceriesMap.set("Rice", 1);
-    groceriesMap.set("Banana", 5);
+    const grocerieMap = new Map();
+    grocerieMap.set("Apples", 10);
+    grocerieMap.set("Tomatoes", 10);
+    grocerieMap.set("Pasta", 1);
+    grocerieMap.set("Rice", 1);
+    grocerieMap.set("Banana", 5);
 
-    return groceriesMap;
+    return grocerieMap;
 }
