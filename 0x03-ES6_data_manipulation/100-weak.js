7db0b38f7535c485c7f95437ba0f1951173c95a2
milemotigo <mtigo@gmail.com>
2023-10-03 16:50:00 +0100

    task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap()
+
+export default function queryAPI(endpoint) {
+    let numOfQueries = weakMap.get(endpoint) || 0;
+    
+    numOfQueries++;
+    if(numOfQueries >= 5){
+        throw new Error('Endpoint load is high')
+    }
+    weakMap.set(endpoint, numOfQueries);
+}
