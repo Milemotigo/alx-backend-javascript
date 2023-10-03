f3248efbd0e6f555b70463647d96fbbd5a79b3ea
milemotigo <mtigo@gmail.com>
2023-10-03 07:15:11 +0100

    new

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+function createInt8TypedArray(length, position, value) {
+    if (position >= length) {
+        throw new Error('Position outside range');
+    }
+    let buffer = new ArrayBuffer(length);
+    let view = new Int8Array(buffer);
+    view[position] = value;
+    return new DataView(buffer);
+}
