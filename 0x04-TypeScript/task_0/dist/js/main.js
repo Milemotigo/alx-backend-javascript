5ce84cebe22ebcd427204c1f9309e3d70c6b0e79
milemotigo <mtigo@gmail.com>
2023-10-05 12:22:15 +0100

    New commit

--- 0x04-TypeScript/task_0/dist/js/main.js
+++ 0x04-TypeScript/task_0/dist/js/main.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var student1 = {
     firstName: 'Favor',
     lastName: 'Maskoli',
@@ -39,4 +54,50 @@ function renderTable() {
 document.addEventListener("DOMContentLoaded", function () {
     renderTable();
 });
+// task 1
+var Teacher = /** @class */ (function () {
+    function Teacher(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this.yearsOfExperience = undefined;
+        this.fullTimeEmployee = false;
+        this.location = '';
+    }
+    return Teacher;
+}());
+// Allow adding arbitrary attributes to the Teacher object:
+// This can be achieved in TypeScript using dynamic property assignment
+var teacher = new Teacher("John", "Doe");
+teacher["contract"] = true;
+console.log(teacher.contract);
+// task 2
+var Directors = /** @class */ (function (_super) {
+    __extends(Directors, _super);
+    function Directors(firstName, lastName, numberOfReports) {
+        var _this = _super.call(this, firstName, lastName) || this;
+        _this.numberOfReports = numberOfReports;
+        return _this;
+    }
+    return Directors;
+}(Teacher));
+var director1 = new Directors('John', 'Doe', 17);
+console.log(director1);
+// task 3
+function printTeacher(firstName, lastName) {
+    var firstNamepart = firstName.charAt(0);
+    return "".concat(firstNamepart, ". ").concat(lastName);
+}
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
 //# sourceMappingURL=main.js.map
--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -56,3 +56,75 @@ function renderTable() {
   document.addEventListener("DOMContentLoaded", () => {
     renderTable();
   });
+
+// task 1
+  class Teacher {
+    protected _firstName: string
+    protected _lastName: string
+    yearsOfExperience?: number
+    fullTimeEmployee: boolean
+    location: string
+    [key: string]: any
+    constructor(firstName: string, lastName: string) {
+        this._firstName = firstName
+        this._lastName = lastName
+        this.yearsOfExperience = undefined
+        this.fullTimeEmployee = false
+        this.location = ''
+    }
+  }
+// Allow adding arbitrary attributes to the Teacher object:
+// This can be achieved in TypeScript using dynamic property assignment
+
+const teacher = new Teacher("John", "Doe")
+teacher["contract"] = true
+console.log(teacher.contract)
+// task 2
+class Directors extends Teacher {
+    numberOfReports: number;
+  
+    constructor(firstName: string, lastName: string, numberOfReports: number) {
+      super(firstName, lastName);
+      this.numberOfReports = numberOfReports;
+    }
+  }
+  
+  const director1 = new Directors('John', 'Doe', 17);
+  console.log(director1);
+// task 3
+  function printTeacher(firstName:string, lastName :string):string {
+    const firstNamepart = firstName.charAt(0)
+    return `${firstNamepart}. ${lastName}`
+  }
+
+// task 4
+interface StudentConstructor{
+    new (firstName: string, lastName: string):StudentClass
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string
+    lastName: string
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    workOnHomework(){
+        return "Currently working";
+    }
+
+    displayName(){
+        return this.firstName;
+    }
+}
+
+
+
+
+
