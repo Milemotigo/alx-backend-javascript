1fcf99df4216f74c91edff02e6843052bd442eed
milemotigo <mtigo@gmail.com>
2023-10-05 14:17:12 +0100

    new commit

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def add(a: float, b: float) ->float:
     '''
-    This is an add function that take to floats as parameters and return them as float
-    types: floats
+    This function takes two arguments and returns their concatenation as a string.
+
+    Args:
+        arg1 (int): The first integer argument.
+        arg2 (str): The second string argument.
+
+    Returns:
+        str: The concatenated string.
     '''
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def concat(str1: str, str2: str) -> str:
     '''
     This function takes two arguments and returns their concatenation as a string.
