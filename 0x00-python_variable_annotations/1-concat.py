77ef47149c1f8520ab27a1de9ff4e2d1a553da1a
milemotigo <mtigo@gmail.com>
2023-10-05 14:12:06 +0100

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def concat(str1: str, str2: str) ->str:
+def concat(str1: str, str2: str) -> str:
     '''
-    This is a function that take two strings as parameters and return their concatination
+    This is a function that takes two strings as parameters and returns their concatenation.
     types: string
     '''
     return str1 + str2
-
-str1 = "egg"
-str2 = "shell"
-
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
