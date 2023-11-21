9546ff39eb8de14c4b5fa04f29fc7f0830429045
milemotigo <mtigo@gmail.com>
2023-11-22 00:08:30 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
-const http = require('http')
+const http = require('http');
 
-let port = 1245
+let port = 1245;
 const app = http.createServer((req, res) => {
 
     res.writeHead(200, {'content-Type' : 'plain/text'});
