1743bfbd0f939bb0cda6423918138a3e82e4e35f
milemotigo <mtigo@gmail.com>
2023-11-21 20:30:25 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,29 @@
-const { count } = require('console')
-const fs = require('fs')
+const fs = require('fs');
 
-const countStudents = (path) => {
+function countStudents(path) {
+  let data;
+  
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 
-    try {
-        const data = fs.readFileSync(path, 'utf8')
-        const line = data.split('\n') - 1
-        console.log(line)
-    } catch (error) {
-        throw new Error('Cannot load the database')
-    }
+  const lines = data.split('\n').filter(Boolean);
+  const students = lines.slice(1); // Ignore header
+
+  console.log(`Number of students: ${students.length}`);
+
+  const fields = { SWE: [], CS: [] };
+
+  students.forEach(student => {
+    const [name, , , field] = student.split(',');
+    if (fields[field]) fields[field].push(name);
+  });
+
+  Object.entries(fields).forEach(([field, students]) => {
+    console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+  });
 }
+
 module.exports = countStudents;
