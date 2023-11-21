3057bad3c62b8f79753a21e2260922eb7acd9ddb
milemotigo <mtigo@gmail.com>
2023-11-22 00:13:58 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 let port = 1245;
 const app = http.createServer((req, res) => {
 
-    res.writeHead(200, {'content-Type' : 'plain/text'});
+    res.writeHead(200, {'content-Type' : 'text/html'});
     res.end('Hello Holberton School!');
 });
 
