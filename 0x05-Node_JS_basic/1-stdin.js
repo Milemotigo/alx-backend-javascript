6cd0202b2e289323b887a1d349d5844728a24219
milemotigo <mtigo@gmail.com>
2023-11-22 00:54:00 +0100

    task 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,22 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
-    console.log(`Your name is: ${name}`);
-    readline.close();
+// readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
+//     console.log(`Your name is: ${name}`);
+//     readline.close();
+// });
+// readline.on('close', () => {
+//     console.log('This important software is now closing')
+// });
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-readline.on('close', () => {
-    console.log('This important software is now closing')
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
