4c4691be494684c52ef2379715eb153120090d10
milemotigo <mtigo@gmail.com>
2023-11-24 00:21:20 +0100

    new commit

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function () {
+    it('should spy the calculateNumber method', function () {
+        const spy = sinon.spy(Utils.prototype, 'calculateNumber');
+        const arg1 = 100,
+            arg2 = 20;
+
+        sendPaymentRequestToApi(arg1, arg2);
+
+        sinon.assert.calledWith(spy, 'SUM', arg1, arg2);
+        spy.restore();
+    });
+});
