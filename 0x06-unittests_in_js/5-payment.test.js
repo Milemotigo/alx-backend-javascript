caa6051f22952f831af005fe12266a71eec05fbb
milemotigo <mtigo@gmail.com>
2023-11-24 01:21:07 +0100

    new commit

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,25 +1,28 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
-const chai = require('chai');
-const expect = chai.expect;
 const sendPaymentRequestToApi = require('./5-payment');
 
-describe('sendPaymentRequestToApi', function () {
-    it('will always return a specific num', function () {
-        let consoleSpy;
+describe('sendPaymentRequestToApi test', () => {
+  let consoleSpy;
 
-        beforeEach(() => {
-          consoleSpy = sinon.spy(console, 'log');
-        });
-      
-        afterEach(() => {
-          consoleSpy.restore();
-        });
-        const stub = sinon.stub(Utils.prototype, 'calculateNumber');
-        const arg1 = 100,
-            arg2 = 20;
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
 
-        stub.withArgs("SUM", arg1, arg2).returns (10);
-        expect(sendPaymentRequestToApi(arg1, arg2)).to.be.equal(10);
-    });
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log the right output for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.callCount(consoleSpy, 1);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log the right output for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
 });
